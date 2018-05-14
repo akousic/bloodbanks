1bcf0f60883813a61f185ba535b0b908092301aa
Kousic Ayyadevara <akousic@gmail.com>
2018-05-14 21:55:16 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('bloodbanks.json', 'utf8'));
+var app = express();
+var port = process.env.PORT || 3000;
+var city = '';
+app.listen(port, () => console.log("running"));
+
+
+app.get('/api/bloodbanks/', (req, res) => {
+    console.log(req.query.city);
+    const bloodbank = obj.records;
+    city = req.query.city;
+    var bloodbankarray = [];
+        for (var i = 0; i < bloodbank.length; i++) {
+            if (bloodbank[i]._city.toLowerCase() == city.toLowerCase()) {
+                var bloodbankobject = new Object();
+                bloodbankobject.bloodbankname = bloodbank[i]._blood_bank_name;
+                bloodbankobject.mobile = bloodbank[i]._mobile;
+                bloodbankobject.address = bloodbank[i]._address;
+                bloodbankarray.push(bloodbankobject);
+            }
+        }
+    res.status(200);
+    res.send(JSON.stringify(bloodbankarray));
+});
